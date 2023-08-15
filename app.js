f4e5a2cde6307bb5347ec75016d07d503305eacb
dhannan12 <110284256+dhannan12@users.noreply.github.com>
2023-08-15 16:43:11 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// Endpoint to receive Zendesk webhook for ticket creation
+app.post("/zendesk/ticket-created", (req, res) => {
+  const ticketData = req.body;
+
+  // For demonstration, we'll just log the ticket data.
+  console.log("Received ticket data:", ticketData);
+
+  res.status(200).send("Received!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
